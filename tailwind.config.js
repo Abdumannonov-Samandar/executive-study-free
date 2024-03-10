e7086c7b7dc35c07a41fe7d40562af1a15cdf28c
Abdumannonov-Samandar <156022272+Abdumannonov-Samandar@users.noreply.github.com>
2024-03-10 12:51:20 +0500

    footer build

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,16 @@ module.exports = {
     },
     colors: {
       blue: "#717CFF",
-      dark: "#373737"
+      dark: "#373737",
+      'danger': "#FF5959",
+      'bl': "5C96FF",
+      orange: "FA706C",
+      green: "70CE88",
+      black: "565656",
     },
     boxShadow: {
-      'box' : '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+      'box' : '0px 0px 77px 0px rgba(0, 0, 0, 0.06)',
+      'boo' : '0px 6px 42px 0px rgba(0, 0, 0, 0.1)',
     },
   },
   plugins: [],
